02bc8e279c45e2e68806eeb707431e5285b51a29
Ricky Brown <ricky@brilliantdeveloping.com>
2016-02-18 15:36:44 -0800

    mount engine at /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
+# Application route
+Rails.application.routes.draw do
+  mount BrilliantCms::Engine, at: '/admin'
+end
+
+# Engine routes
 BrilliantCms::Engine.routes.draw do
+
   scope "/:content_class" do
     resources :entries
   end
+
+  root 'dashboard#index'
 end
